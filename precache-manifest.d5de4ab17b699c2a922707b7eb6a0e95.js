0d3ef3365c92ca5d396a204e34a73f11568dceae
Rostyslav <rostyk1331@gmail.com>
2019-11-26 14:45:47 +0200

    Updates

---
+++ precache-manifest.d5de4ab17b699c2a922707b7eb6a0e95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd42d1dec630675ba35368995c10989",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "9d9b89f790cd0cf999b7",
+    "url": "/pokedex/static/css/main.5ebcf7ab.chunk.css"
+  },
+  {
+    "revision": "dac18e396ce00e3c712b",
+    "url": "/pokedex/static/js/2.1f3c8142.chunk.js"
+  },
+  {
+    "revision": "9d9b89f790cd0cf999b7",
+    "url": "/pokedex/static/js/main.6ab2ea5d.chunk.js"
+  },
+  {
+    "revision": "eadf7279b87926518061",
+    "url": "/pokedex/static/js/runtime-main.0647a4aa.js"
+  }
+]);
